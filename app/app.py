6ce0ae296d226d4713352c8b7016d50e1b47f3d8
Gabean01 <132403837+Gabean01@users.noreply.github.com>
2023-08-23 20:50:05 +0000

    Routes finished and the swagger model was implemented to the CRUD

--- app/app.py
+++ app/app.py
@@ -7,16 +7,20 @@ import traceback
 from flask_restx import Api
 
 
+
+
 app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/backend_challenge'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 
 db = SQLAlchemy(app)
-
-
-#from app import routes
+migrate = Migrate(app, db)
+api = Api(app)
 
 with app.app_context():
     db.create_all()
+
+if __name__ == '__main__':
+    app.run()    
--- app/routes.py
+++ app/routes.py
@@ -1,170 +1,204 @@
 from flask import request, jsonify, abort
+from flask_restx import Api, Resource
 from app import app, db
 from app.models import User, Post, Comment
 
 #USERS
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    user_list = [{"id": user.id, "username": user.username, "email": user.email} for user in users]
-    return jsonify(user_list)
+users_ns = Namespace('users', description='User related operations')
+
+@users_ns.route('/')
+class UsersResource(Resource):
+    @users_ns.doc(description="Gets all users")
+    def get(self):
+        users = User.query.all()
+        user_list = [{"id": user.id, "username": user.username, "email": user.email} for user in users]
+        return jsonify(user_list)
+
+@users_ns.doc(description="Create a new user")
+    @users_ns.expect(user_model)  
+    def post(self):
+        data = request.get_json()
+        if "username" not in data or "email" not in data:
+            return jsonify({"error": "Missing username or email"}), 400
+
+        new_user = User(username=data["username"], email=data["email"])
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"message": "User created successfully"}), 201
 
 
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.get(user_id)
-    if not user:
-        abort(404)
-    return jsonify({"id": user.id, "username": user.username, "email": user.email}) 
+@users_ns.route('/<int:user_id>')
+class UserResource(Resource):
+    @users_ns.doc(description="Gets a user by ID")
+    @users_ns.param('user_id')
+    def get(self, user_id):
+        user = User.query.get(user_id)
+        if not user:
+            abort(404)
+        return jsonify({"id": user.id, "username": user.username, "email": user.email}) 
 
 
-@app.route('/users', methods=['POST']) 
-def created_user():
-    data = request.get_json()
-    if "username" not in data or "email" not in data:
-        return jsonify({"error": "Missing username or email"}), 400
+@users_ns.doc(description="Update a user by ID")
+    @users_ns.param('user_id')
+    @users_ns.expect(user_model)  
+    def put(self, user_id):
+        user = User.query.get(user_id)
+        if not user:
+            abort(404)
 
-    new_user = User(username=data["username"], email=data["email"])
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({"message": "User created successfully"}), 201
+        data = request.get_json()
+        if "username" not in data or "email" not in data:
+            return jsonify({"error": "Missing username or email"}), 400
 
-
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    user = User.query.get(user_id)
-    if not user:
-        abort(404)
-
-    data = request.get_json()
-    if "username" not in data or "email" not in data:
-        return jsonify({"error": "Missing username or email"}), 400
-
-    user.username = data["username"]
-    user.email = data["email"]
-    db.session.commit()
-    return jsonify({"message": "User updated successfully"})
+        user.username = data["username"]
+        user.email = data["email"]
+        db.session.commit()
+        return jsonify({"message": "User updated successfully"})
 
 
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    user = User.query.get(user_id)
-    if not user:
-        abort(404)
+ @users_ns.doc(description="Delete a user by ID")
+    @users_ns.param('user_id')
+    def delete(self, user_id):
+        user = User.query.get(user_id)
+        if not user:
+            abort(404)
 
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({"message": "User deleted successfully"})
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": "User deleted successfully"})
 
 
 
 #POSTS
 
-@app.route('/posts', methods=['GET'])
-def get_posts():
-    posts = Post.query.all()
-    post_list = [{"id": post.id, "title": post.title, "content": post.content} for post in posts]
-    return jsonify(post_list)
+posts_ns = Namespace('posts', description='Post-Related Operations')
+
+@posts_ns.route('/')
+class PostsResource(Resource):
+    @posts_ns.doc(description="Obtiene todas las publicaciones")
+    def get(self):
+        posts = Post.query.all()
+        post_list = [{"id": post.id, "title": post.title, "content": post.content} for post in posts]
+        return jsonify(post_list)
 
 
-@app.route('/posts/<int:post_id>', methods=['GET'])   
- def get_posts():
-    posts = Post.query.get(post_id)
-    if not post:
-        abort(400)
-    return jsonify({"id": post.id, "title": post.title, "content": post.content} for post in posts)
+@posts_ns.doc(description="Create a new post")
+    @posts_ns.expect(post_model)  
+    def post(self):
+        data = request.get_json()
+        if "title" not in data or "content" not in data:
+            return jsonify({"error": "Missing title or content"}), 400
+
+        new_post = Post(title=data["title"], content=data["content"])
+        db.session.add(new_post)
+        db.session.commit()
+        return jsonify({"message": "Post created successfully"}), 201
+
+@posts_ns.route('/<int:post_id>')
+class PostResource(Resource):
+    @posts_ns.doc(description="Get a post by ID")
+    @posts_ns.param('post_id')
+    def get(self, post_id):
+        post = Post.query.get(post_id)
+        if not post:
+            abort(404)
+        return jsonify({"id": post.id, "title": post.title, "content": post.content}) 
 
 
-@app.route('/posts', methods=['POST'])
-def create_post():
-    data = request.get_json()
-    if "title" not in data or "content" not in data:
-        return jsonify({"error": "Missing title or content"}), 400
+@posts_ns.doc(description="Update a post by ID")
+    @posts_ns.param('post_id')
+    @posts_ns.expect(post_model)  
+    def put(self, post_id):
+        post = Post.query.get(post_id)
+        if not post:
+            abort(404)
 
-    new_post = Post(title=data["title"], content=data["content"])
-    db.session.add(new_post)
-    db.session.commit()
-    return jsonify({"message": "Post created successfully"}), 201
+        data = request.get_json()
+        if "title" not in data or "content" not in data:
+            return jsonify({"error": "Missing title or content"}), 400
 
-
-@app.route('/posts/<int:post_id>', methods=['PUT'])
-def update_post(post_id):
-    post = Post.query.get(post_id)
-    if not post:
-        abort(404)
-
-    data = request.get_json()
-    if "title" not in data or "content" not in data:
-        return jsonify({"error": "Missing title or content"}), 400
-
-    post.title = data["title"]
-    post.content = data["content"]
-    db.session.commit()
-    return jsonify({"message": "Post updated successfully"})
+        post.title = data["title"]
+        post.content = data["content"]
+        db.session.commit()
+        return jsonify({"message": "Post updated successfully"})
 
 
-@app.route('/posts/<int:post_id>', methods=['DELETE'])
-def delete_post(post_id):
-    post = Post.query.get(post_id)
-    if not post:
-        abort(404)
 
-    db.session.delete(post)
-    db.session.commit()
-    return jsonify({"message": "Post deleted successfully"})
+@posts_ns.doc(description="Delete a post by ID")
+    @posts_ns.param('post_id')
+    def delete(self, post_id):
+        post = Post.query.get(post_id)
+        if not post:
+            abort(404)
+
+        db.session.delete(post)
+        db.session.commit()
+        return jsonify({"message": "Post deleted successfully"})
+
 
 
 #COMMENTS
 
-@app.route('/comments', methods=['GET'])
-def get_comments():
-    comments = Comment.query.all()
-    comment_list = [{"id": comment.id, "text": comment.text} for comment in comments]
-    return jsonify(comment_list)
+comments_ns = Namespace('comments', description='Comment Related Operations')
+
+@comments_ns.route('/')
+class CommentsResource(Resource):
+    @comments_ns.doc(description="Gets all comments")
+    def get(self):
+        comments = Comment.query.all()
+        comment_list = [{"id": comment.id, "text": comment.text} for comment in comments]
+        return jsonify(comment_list)
+
+@comments_ns.doc(description="Create a new comment")
+    @comments_ns.expect(comment_model)  
+    def post(self):
+         data = request.get_json()
+        if "text" not in data:
+            return jsonify({"error": "Missing text"}), 400
+
+        new_comment = Comment(text=data["text"])
+        db.session.add(new_comment)
+        db.session.commit()
+        return jsonify({"message": "Comment created successfully"}), 201
 
 
-@app.route('/comments/<int:comment_id>', methods=['GET'])
-def get_comment(comment_id):
-    comment = Comment.query.get(comment_id)
-    if not comment:
-        abort(404)
-    return jsonify({"id": comment.id, "text": comment.text})
+@comments_ns.route('/<int:comment_id>')
+class CommentResource(Resource):
+    @comments_ns.doc(description="Gets a comment by ID")
+    @comments_ns.param('comment_id')
+    def get(self, comment_id):
+        comment = Comment.query.get(comment_id)
+        if not comment:
+            abort(404)
+        return jsonify({"id": comment.id, "text": comment.text}) 
 
 
-@app.route('/comments', methods=['POST'])
-def create_comment():
-    data = request.get_json()
-    if "text" not in data:
-        return jsonify({"error": "Missing text"}), 400
+@comments_ns.doc(description="Update a comment by ID")
+    @comments_ns.param('comment_id')
+    @comments_ns.expect(comment_model)  
+    def put(self, comment_id):
+         comment = Comment.query.get(comment_id)
+        if not comment:
+            abort(404)
 
-    new_comment = Comment(text=data["text"])
-    db.session.add(new_comment)
-    db.session.commit()
-    return jsonify({"message": "Comment created successfully"}), 201
+        data = request.get_json()
+        if "text" not in data:
+            return jsonify({"error": "Missing text"}), 400
 
-
-@app.route('/comments/<int:comment_id>', methods=['PUT'])
-def update_comment(comment_id):
-    comment = Comment.query.get(comment_id)
-    if not comment:
-        abort(404)
-
-    data = request.get_json()
-    if "text" not in data:
-        return jsonify({"error": "Missing text"}), 400
-
-    comment.text = data["text"]
-    db.session.commit()
-    return jsonify({"message": "Comment updated successfully"})
+        comment.text = data["text"]
+        db.session.commit()
+        return jsonify({"message": "Comment updated successfully"})
 
 
-@app.route('/comments/<int:comment_id>', methods=['DELETE'])
-def delete_comment(comment_id):
-    comment = Comment.query.get(comment_id)
-    if not comment:
-        abort(404)
 
-    db.session.delete(comment)
-    db.session.commit()
-    return jsonify({"message": "Comment deleted successfully"})
+ @comments_ns.doc(description="Delete a comment by ID")
+    @comments_ns.param('comment_id')
+    def delete(self, comment_id):
+        comment = Comment.query.get(comment_id)
+        if not comment:
+            abort(404)
+
+        db.session.delete(comment)
+        db.session.commit()
+        return jsonify({"message": "Comment deleted successfully"})
